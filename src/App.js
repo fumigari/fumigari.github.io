2fd64ea28812d7c9fa5d1ee283b897877f1934b7
Ariany Ferreira <arianysferreira1@gmail.com>
2023-10-05 13:57:33 -0300

    refactor: update app and app style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
+import React, {useContext} from 'react';
 import './App.css';
 import Home from './components/home/Home';
 import Sidebar from './components/sidebar/Sidebar';
+import { ShowSidebarContext } from './hooks/ShowSidebar';
 
 function App() {
+  const { openSidebar } = useContext(ShowSidebarContext);
+
   return (
     <>
-    <Sidebar />
-    <main className='main'>
-      <Home />
-    </main>
+      <Sidebar />
+      <main className={`main ` + openSidebar}>
+        <Home />
+      </main>
     </>
   );
 }
