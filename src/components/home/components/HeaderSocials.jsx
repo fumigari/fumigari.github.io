a21c1e1e0ab34c11affed93466a0c948a49259fc
Ariany Ferreira <arianysferreira1@gmail.com>
2023-10-05 14:01:53 -0300

    feat: add header socials component

---
+++ src/components/home/components/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HeaderSocials = () => {
+	return (
+		<div className="home__socials">
+			<a href="https://linkedin.com/in/arianysferreira/" className="home__social-link" target="_blank" rel="noreferrer noopener">
+				<i className="fa-brands fa-linkedin fa-lg"></i>
+			</a>
+
+			<a href="https://github.com/fumigari/" className="home__social-link" target="_blank" rel="noreferrer noopener">
+				<i className="fa-brands fa-github fa-lg"></i>
+			</a>
+
+			<a href="https://instagram.com/fumigari/" className="home__social-link" target="_blank" rel="noreferrer noopener">
+				<i className="fa-brands fa-instagram fa-lg"></i>
+			</a>
+		</div>
+	);
+}
+
+export default HeaderSocials;
