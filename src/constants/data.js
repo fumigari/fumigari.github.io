b80a19934de042fad582c67fa1e8faa3fddff776
Ariany Ferreira <arianysferreira1@gmail.com>
2024-03-22 09:41:05 -0300

    chore: add constants

---
+++ src/constants/data.js
@@ -0,0 +1,84 @@
+export const userData = {
+	githubUsername: "fumigari",
+	name: "Ari Ferreira",
+	designation: "Software Developer",
+	avatarUrl: "/avatar.svg",
+	email: "arianysferreira1@gmail.com",
+	phone: "+55 996496997",
+	address: "Recife, Pernambuco, Brazil",
+	projects: [
+		{
+			title: "Tip Calculator",
+			link: "https://ariferreira.com/tip-calculator",
+			imgUrl: "/tip-calc.png",
+		},
+		{
+			title: "Password Component",
+			link: "https://ariferreira.com/password-component/",
+			imgUrl: "/tip-calc.png",
+		},
+	],
+	about: {
+		title:
+			"I'm a software developer .....",
+		description: [
+			`I'm Ari, a developer based in Recife/Brazil 
+			passionate about turning ideas into creative solutions. 
+			With solid experience in both frontend and backend, 
+			my goal is to create engaging and functional digital experiences. `,
+		],
+	},
+	experience: [
+		{
+			title: "Software Developer",
+			company: "Neurotech",
+			year: "2023 - Present",
+			companyLink: "https://www.neurotech.com.br/",
+			desc: "Development and maintenance for credit, risk and insurance assessment",
+		},
+		{
+			title: "Front-End Developer",
+			company: "Thoughtworks",
+			year: "2021 - 2023",
+			companyLink: "https://www.thoughtworks.com/pt-br",
+			desc: "Development and maintenance web interface for product pages for an US-based e-commerce company.",
+		},
+		{
+			title: "Full Stack Developer",
+			company: "Thoughtworks",
+			year: "2021",
+			companyLink: "https://www.thoughtworks.com/pt-br",
+			desc: "A global development program with learning objectives from back-end to front-end.",
+		},
+		{
+			title: "Back-End Developer",
+			company: "Avanade",
+			year: "2021",
+			companyLink: "https://www.avanade.com/pt-br",
+			desc: "Development and management the back-end project's life cycle, focused on increasing quality deliveries and reducing risks in the client's HR department.",
+		},
+		{
+			title: "Back-End Developer Intern",
+			company: "Avanade",
+			year: "2019 - 2021",
+			companyLink: "https://www.avanade.com/pt-br",
+			desc: "Internship to develop and maintain back-end applications for a HR department.",
+		},
+		{
+			title: "Graduation",
+			company: "UFRPE, Recife.",
+			year: "2014 - 2021",
+			companyLink: "https://www.ufrpe.br/",
+			desc: "Bachelor’s Degree in Computer Science.",
+		},
+	],
+	resumeUrl:
+		"https://drive.google.com/file/d/1e7G7405YVzLAVjIgnEHRGhMPmMqFMC77/view?usp=sharing",
+	socialLinks: {
+		instagram: "https://instagram.com/fumigari",
+		linkedin: "https://linkedin.com/in/arianysferreira",
+		github: "https://github.com/fumigari",
+	},
+};
+
+export default userData;
